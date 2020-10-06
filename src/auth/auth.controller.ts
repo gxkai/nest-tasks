3f2527f5370421183e19e3dd519ffcc9fe54306c
Damodar Lohani <dlohani48@gmail.com>
2020-10-06 20:29:46 +0545

    jwt strategy setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -16,5 +17,11 @@ export class AuthController {
     @Post('signin')
     signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialDto);
+    }
+
+    @Post('test')
+    @UseGuards(AuthGuard())
+    test(@Req() req) {
+        console.log(req);
     }
 }
