ae48b7990d97ad300ea6b05c9179b9da5270c044
Damodar Lohani <dlohani48@gmail.com>
2020-10-06 18:37:59 +0545

    create user - signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,14 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
-
+    async signUp(authCredential: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredential;
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
 }
